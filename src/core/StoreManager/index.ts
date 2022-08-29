998e55b9274a657046745e8d934032117e03fa9b
Strong <justbefree@126.com>
2022-08-29 15:44:31 +0800

    feat: 支持多个action共享一个mutation

--- src/core/StoreManager/index.ts
+++ src/core/StoreManager/index.ts
@@ -2,7 +2,7 @@
  * @Author: Just be free
  * @Date:   2020-07-27 16:02:38
  * @Last Modified by:   Just be free
- * @Last Modified time: 2022-07-18 18:35:29
+ * @Last Modified time: 2022-08-29 15:43:50
  * @E-mail: justbefree@126.com
  */
 import { APIobject, State } from "./types";
@@ -14,15 +14,19 @@ import { ActionContext } from "vuex/types";
 class StoreManager {
   private _moduleName: string;
   private _actionName: string;
+  private _actionNames: Array<string>;
   private _API: APIobject;
   private _states: AnyObject;
   private _action: AnyObject;
   private _mutation: AnyObject;
   private _getters: AnyObject;
   private _axiosConfig: AnyObject;
+  private _sharedMutationType: string;
+  private sharedMutation: boolean;
   constructor(moduleName: string, axiosConfig: AnyObject = {}) {
     this._moduleName = moduleName;
     this._actionName = "";
+    this._actionNames = [] as Array<string>;
     this._API = {};
     this._states = {};
     this._action = {};
@@ -30,12 +34,14 @@ class StoreManager {
     this._getters = {};
     this._axiosConfig = axiosConfig;
     this.setApi();
+    this._sharedMutationType = "SHARED_MUTATION_TYPES";
+    this.sharedMutation = false;
   }
   private setApi(): void {
     try {
       this._API = {
         // eslint-disable-next-line
-        ...require(`@/overwrite/${this._moduleName}/store`)["API"],
+        ...require(`@/custom/${this._moduleName}/store`)["API"],
         // eslint-disable-next-line
         ...require(`@/applications/${this._moduleName}/store`)["API"],
       };
@@ -55,7 +61,10 @@ class StoreManager {
     return this._states;
   }
   public hasMutation(actionName: string): boolean {
-    return hasProperty(this._mutation, getType(this._moduleName, actionName));
+    const mutationType = this.sharedMutation
+      ? this._sharedMutationType
+      : getType(this._moduleName, actionName);
+    return hasProperty(this._mutation, mutationType);
   }
   protected httpSuccessCallback(args: AnyObject | string): void {
     console.log("http success callback", args);
@@ -67,14 +76,88 @@ class StoreManager {
     console.log("http params modifer", args);
     return args;
   }
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   protected setRequestHeaders(uri: string, params: AnyObject): AnyObject {
-    console.log(`set http request headers; url = ${uri}, params = ${params}`);
+    console.log("set http request headers");
     return {};
   }
   protected mergeConfig(uri: string, params: AnyObject): AnyObject {
     const config = this.setRequestHeaders(uri, params);
     return { ...this._axiosConfig, ...config };
   }
+
+  public actions(actionObject: AnyObject): StoreManager {
+    const actionNameArr = Object.keys(actionObject);
+    this._actionNames = actionNameArr;
+    actionNameArr.forEach((actionName: string) => {
+      const type = actionObject[actionName].type;
+      const url = actionObject[actionName].url;
+      this._action[actionName] = (
+        context: ActionContext<State, Anything>,
+        args: AnyObject
+      ) => {
+        this.ajax(url, type, args, actionName, context);
+      };
+    });
+    return this;
+  }
+
+  public mutations(callback: Callback): StoreManager {
+    this._actionNames.forEach((actionName: string) => {
+      const mutationName = this.sharedMutation
+        ? this._sharedMutationType
+        : getType(this._moduleName, actionName);
+      this._mutation = {
+        ...this._mutation,
+        [mutationName](state: State, payload: AnyObject) {
+          callback({ state, payload });
+        },
+      };
+    });
+    return this;
+  }
+
+  public setSharedMutationType(mutationType?: string): StoreManager {
+    if (mutationType) {
+      this._sharedMutationType = mutationType;
+    }
+    this.sharedMutation = true;
+    return this;
+  }
+
+  private ajax(
+    url: string,
+    method: HttpMethodTypes,
+    args: AnyObject,
+    actionName: string,
+    context: ActionContext<State, Anything>
+  ) {
+    const { params } = args;
+    return Http(method)(
+      url,
+      this.httpParamsModifier(params),
+      this.mergeConfig(url, params)
+    )
+      .then((res) => {
+        if (this.hasMutation(actionName)) {
+          let mutationTypes: string = getType(this._moduleName, actionName);
+          if (this.sharedMutation) {
+            mutationTypes = this._sharedMutationType;
+          }
+          context.commit(mutationTypes, {
+            ...args,
+            res,
+          });
+        }
+        this.httpSuccessCallback(res);
+        return Promise.resolve(res);
+      })
+      .catch((err) => {
+        this.httpFailCallback(err);
+        return Promise.reject(err);
+      });
+  }
+
   public action(
     actionName: string,
     async = false,
@@ -86,25 +169,7 @@ class StoreManager {
       args: AnyObject
     ) => {
       if (async) {
-        const { params } = args;
-        return Http(method)(
-          this._API[actionName],
-          this.httpParamsModifier(params)
-        )
-          .then((res) => {
-            if (this.hasMutation(actionName)) {
-              context.commit(getType(this._moduleName, actionName), {
-                ...args,
-                res,
-              });
-            }
-            this.httpSuccessCallback(res);
-            return Promise.resolve(res);
-          })
-          .catch((err: Anything) => {
-            this.httpFailCallback(err);
-            return Promise.reject(err);
-          });
+        this.ajax(this._API[actionName], method, args, actionName, context);
       } else {
         context.commit(getType(this._moduleName, actionName), { ...args });
       }
@@ -145,7 +210,7 @@ class StoreManager {
     return this._getters;
   }
 
-  public register(args: AnyObject): StoreManager {
+  public register(args: AnyObject = {}): StoreManager {
     const state = args.state;
     this.setState(state);
     return this;
